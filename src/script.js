1d2195fcc25f8f9d028027bcbfab6ac9cb962a1b
Yauvan <54529920+VirtualMonk-108@users.noreply.github.com>
2023-12-28 15:39:39 +0100

    perspective camera notes

--- src/script.js
+++ src/script.js
@@ -20,9 +20,22 @@ const sizes = {
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+// the first parameter is the field of view and it's vertical
+// the second parameter is the aspect ratio and it's horizontal - the width divided by the height
+// the third parameter is the near clipping plane - the closest point that the camera can see
+// the fourth parameter is the far clipping plane - the furthest point that the camera can see
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+
+camera.lookAt(mesh.position) // We make the camera look at the mesh
 scene.add(camera)
+
+// the problem with perspective camera is that it distorts the objects 
+// that are closer to the camera and makes them look bigger while the objects that are f
+// further away look smaller
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -31,19 +44,17 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 // Time We create a clock object
-// const clock = new THREE.Clock()
-
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+const clock = new THREE.Clock()
 
 // Animation
 const tick = () => {
 
     // Elapsed time
-    //const elapsedTime = clock.getElapsedTime() // We get the elapsed time since the clock was created
+    const elapsedTime = clock.getElapsedTime() // We get the elapsed time since the clock was created
 
     // Update objects
     // mesh.rotation.y = elapsedTime // We rotate the mesh on the y axis
-    // mesh.rotation.x = elapsedTime // We rotate the mesh on the y axis
+    mesh.rotation.y = elapsedTime // We rotate the mesh on the y axis
 
     // Render - we moved this from outside the tick function to inside it
     renderer.render(scene, camera)
